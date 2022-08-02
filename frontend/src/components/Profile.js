c85b5eb29490fe99bfb7604c14a1e1940877221c
Adam-AbJab <89073692+Adam-AbJab@users.noreply.github.com>
2022-08-02 08:45:06 +0300

    fix profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -130,7 +130,10 @@ class Profile extends React.Component {
       >
         <div className="container">
           <div className="row p-4 text-center">
-            <div className="user-info col-xs-12 col-md-8 offset-md-2">
+            <div
+              className="user-info col-xs-12 col-md-8 offset-md-2"
+              style={{ maxWidth: document.documentElement.clientWidth, minWidth: '0px' }}
+            >
               <img
                 src={profile.image}
                 className="user-img"
